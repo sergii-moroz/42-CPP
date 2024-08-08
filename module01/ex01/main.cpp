580fb2f3573196cb572171e89360b0d25ef36651
Sergii Moroz <smoroz@3-G-2.42heilbronn.de>
2024-08-08 13:55:33 +0200

    module01: ex01: CREATE main.cpp, Makefile

---
+++ module01/ex01/main.cpp
@@ -0,0 +1,16 @@
+#include "Zombie.hpp"
+
+void	check_leaks(void)
+{
+	system("leaks zombieHorde");
+}
+
+int	main(void)
+{
+	atexit(check_leaks);
+	Zombie	*horde = zombieHorde(10, "Bob");
+	for (int i=0; i < 10; i++)
+		horde[i].announce();
+	delete [] horde;
+	return (0);
+}
