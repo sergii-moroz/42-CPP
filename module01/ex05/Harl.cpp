056f5c3410f8e010803ed489c504f008a847e6c6
sergii-moroz <70237603+sergii-moroz@users.noreply.github.com>
2024-08-10 13:18:52 +0200

    module01: ex05: Create files with basic code

---
+++ module01/ex05/Harl.cpp
@@ -0,0 +1,26 @@
+#include "Harl.hpp"
+
+void	Harl::complain( std::string level)
+{
+
+}
+
+void	Harl::debug( void )
+{
+
+}
+
+void	Harl::info( void )
+{
+
+}
+
+void	Harl::warning( void )
+{
+
+}
+
+void	Harl::error( void )
+{
+
+}
