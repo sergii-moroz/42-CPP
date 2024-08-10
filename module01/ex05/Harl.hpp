8a779522d87378ecb3b048c5e1893b9617857a3e
sergii-moroz <70237603+sergii-moroz@users.noreply.github.com>
2024-08-10 14:23:29 +0200

    module 01: ex05: MODIFY: Harl class

--- module01/ex05/Harl.hpp
+++ module01/ex05/Harl.hpp
@@ -1,17 +1,28 @@
 #ifndef HARL_H
 # define HARL_H
 
+#  define BLACK		"\033[1;30m"
+#  define RED		"\033[1;31m"
+#  define GREEN		"\033[1;32m"
+#  define YELLOW	"\033[1;33m"
+#  define BLUE		"\033[1;34m"
+#  define MAGENTA	"\033[1;35m"
+#  define CYAN		"\033[1;36m"
+#  define WHITE		"\033[1;37m"
+#  define RESET		"\033[0m"
+
+#include <iostream>
 #include <string>
 
 class Harl
 {
 	private:
-		void	debug( void );
-		void	info( void );
-		void	warning( void );
-		void	error( void );
+		void	debug( void ) const;
+		void	info( void ) const;
+		void	warning( void ) const;
+		void	error( void ) const;
 	public:
-		void	complain( std::string level );
+		void	complain( std::string level ) const;
 };
 
 #endif
