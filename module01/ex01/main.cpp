5d109ed4a8e0776cf8a0c044a51de00aa2862c69
smoroz <smoroz@student.42heilbronn.de>
2024-08-12 11:29:47 +0200

    module01:ex01: delete check_leaks function

--- module01/ex01/main.cpp
+++ module01/ex01/main.cpp
@@ -1,13 +1,7 @@
 #include "Zombie.hpp"
 
-void	check_leaks(void)
-{
-	system("leaks zombieHorde");
-}
-
 int	main(void)
 {
-	// atexit(check_leaks);
 	Zombie	*horde = zombieHorde(10, "Bob");
 	for (int i=0; i < 10; i++)
 		horde[i].announce();
