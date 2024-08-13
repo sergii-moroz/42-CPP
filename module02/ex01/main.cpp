a54e17caa9b095250f9b66f4c04881673032f2ad
smoroz <smoroz@student.42heilbronn.de>
2024-08-13 21:45:30 +0200

    module02: ex01: CREATE main.cpp Makefile

---
+++ module02/ex01/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/13 20:46:41 by smoroz            #+#    #+#             */
+/*   Updated: 2024/08/13 21:28:28 by smoroz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main( void )
+{
+	Fixed		a;
+	Fixed const	b( 10 );
+	Fixed const	c( 42.42f );
+	Fixed		d( b );
+
+	a = Fixed( 1234.4321f );
+
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+
+	std::cout << "a is " << a.toInt() << " as Integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as Integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as Integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as Integer" << std::endl;
+	return (0);
+}
