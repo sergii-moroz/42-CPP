dd67dc3214efb034ea7601190481edc4d935c8d3
smoroz <smoroz@student.42heilbronn.de>
2024-08-14 20:13:59 +0200

    module02: ex02: Add second test to main.cpp

--- module02/ex02/main.cpp
+++ module02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 20:46:41 by smoroz            #+#    #+#             */
-/*   Updated: 2024/08/13 21:58:16 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/08/14 19:47:22 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,52 @@ int	main( void )
 	std::cout << b << std::endl;
 
 	std::cout << Fixed::max(a, b) << std::endl;
+
+
+// Second test
+/*
+	Fixed		a( 4.8f );
+	Fixed const	b( 1.5f );
+
+	std::cout << "a = " << a << std::endl;
+	std::cout << "b = " << b << std::endl;
+	std::cout << "a + b = " << a + b << std::endl;
+	std::cout << "a - b = " << a - b << std::endl;
+	std::cout << "a * b = " << a * b << std::endl;
+	std::cout << "a / b = " << a / b << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "a < b : " << (a < b) << std::endl;
+	std::cout << "a > b : " << (a > b) << std::endl;
+	std::cout << "a <= b : " << (a <= b) << std::endl;
+	std::cout << "a >= b : " << (a >= b) << std::endl;
+	std::cout << "a == b : " << (a == b) << std::endl;
+	std::cout << "a != b : " << (a != b) << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "a = " << a << std::endl;
+	std::cout << "++a : " << ++a << std::endl;
+	std::cout << "a = " << a << std::endl;
+	std::cout << "--a : " << --a << std::endl;
+	std::cout << "a = " << a << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "a = " << a << std::endl;
+	std::cout << "a++ : " << a++ << std::endl;
+	std::cout << "a = " << a << std::endl;
+	std::cout << "a-- : " << a-- << std::endl;
+	std::cout << "a = " << a << std::endl;
+	std::cout << std::endl;
+
+	Fixed c(13.7f);
+	std::cout << "a = " << a << std::endl;
+	std::cout << "b = " << b << std::endl;
+	std::cout << "c = " << c << std::endl;
+	std::cout << "min(a, c): " << Fixed::min(a, c) << std::endl;
+	std::cout << "min(a, c): " << Fixed::max(a, c) << std::endl;
+	std::cout << "max(a, b): " << Fixed::min(a, b) << std::endl;
+	std::cout << "max(a, b): " << Fixed::max(a, b) << std::endl;
+*/
 
 	return (0);
 }
