49f9a8301a902642485b1e9f65000fc6049be619
Sergii Moroz <smoroz@3-F-5.42heilbronn.de>
2024-08-15 14:53:42 +0200

    module02: ex02: trying to writting Points's getter/setter

--- module02/ex03/Point.cpp
+++ module02/ex03/Point.cpp
@@ -6,14 +6,68 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 11:38:06 by smoroz            #+#    #+#             */
-/*   Updated: 2024/08/15 11:44:05 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/08/15 14:49:22 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
+// =========================================================
+// constructors
+// =========================================================
+
 Point::Point( void ) : _x(0), _y(0) {}
 
 Point::Point( float const & x, float const & y) : _x(x), _y(y) {}
 
+Point::Point( Point const & p)
+{
+	*this = p;
+}
+
+// =========================================================
+// destructor
+// =========================================================
+
 Point::~Point( void ) {}
+
+// =========================================================
+// member functions
+// =========================================================
+
+Fixed	Point::getX( void ) const
+{
+	return ( _x );
+}
+
+Fixed	Point::getY( void ) const
+{
+	return ( _y );
+}
+
+void	Point::setX( Fixed const & fx )
+{
+	_x.setRawBits;
+}
+
+void	Point::setY( Fixed const & fx )
+{
+	_y = fx;
+}
+
+void	Point::setXY( Fixed const & fx, Fixed cont & fy )
+{
+	_x = Fixed();
+	_y = Fixed();
+}
+
+// =========================================================
+// assignment operator "=" overloading
+// =========================================================
+
+Point &	Point::operator=(Point const & p)
+{
+	setX( p.getX() );
+	setY( p.getY() )
+	return ( *this );
+}
--- module02/ex03/Point.hpp
+++ module02/ex03/Point.hpp
@@ -6,7 +6,7 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 08:30:34 by smoroz            #+#    #+#             */
-/*   Updated: 2024/08/15 11:42:04 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/08/15 14:39:27 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,16 @@ class Point
 	public:
 		Point();
 		Point( float const & x, float const & y );
+		Point( Point const & p);
 		~Point();
+		// member functions
+		Fixed	getX( void ) const;
+		Fixed	getY( void ) const;
+		void	setX( Fixed const & fx );
+		void	setY( Fixed const & fx );
+		void	setXY( Fixed const & fx, Fixed const & fy );
+		// overloading
+		Point &	operator=(Point const & p);
 
 };
 
