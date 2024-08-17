f2f7a8abc4c31ad5ed7121369e9bbdef2c8c2d2a
smoroz <smoroz@student.42heilbronn.de>
2024-08-17 14:56:11 +0200

    module02: ex03: CREATE makefile main.cpp

---
+++ module02/ex03/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/15 21:42:35 by smoroz            #+#    #+#             */
+/*   Updated: 2024/08/17 14:51:10 by smoroz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Point.hpp"
+
+int	main( void )
+{
+	Point	a(0.0f,0.0f);
+	Point	b(1.0f, 0.0f);
+	Point	c(0.0f, 1.0f);
+	Point	p(0.25f, 0.25f);
+
+	std::cout << "A: " << a << std::endl;
+	std::cout << "B: " << b << std::endl;
+	std::cout << "C: " << c << std::endl;
+	std::cout << "P: " << p << std::endl;
+
+	return (0);
+}
