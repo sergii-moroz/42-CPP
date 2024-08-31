a4c7722c2ca2fcab529ecea11ff4d4454dbe5b3f
sergii-moroz <70237603+sergii-moroz@users.noreply.github.com>
2024-08-31 13:16:00 +0200

    module03: ex01: copy ClapTrap.cpp from previous exercise

---
+++ module03/ex01/ClapTrap.cpp
@@ -0,0 +1,181 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/20 11:48:32 by smoroz            #+#    #+#             */
+/*   Updated: 2024/08/28 20:41:21 by smoroz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+// =========================================================
+// constructors
+// =========================================================
+
+ClapTrap::ClapTrap( void ) : _name("noname"), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+	std::cout << "ClapTrap: Default constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string const & name) : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+	std::cout << "ClapTrap: Constructor with name \"" << _name << "\" called" << std::endl;
+}
+
+
+ClapTrap::ClapTrap( ClapTrap const & copy)
+{
+	std::cout << "ClapTrap: Copy constructor called" << std::endl;
+	*this = copy;
+}
+
+// =========================================================
+// destructor
+// =========================================================
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "ClapTrap: Destructor called" << std::endl;
+}
+
+// =========================================================
+// getters / setters
+// =========================================================
+
+std::string		ClapTrap::getName() const
+{
+	return ( _name );
+}
+
+unsigned int	ClapTrap::getHitPoints() const
+{
+	return ( _hitPoints );
+}
+
+unsigned int	ClapTrap::getEnergyPoints() const
+{
+	return ( _energyPoints );
+}
+
+unsigned int	ClapTrap::getAttackDamage() const
+{
+	return ( _attackDamage );
+}
+
+void	ClapTrap::setName( std::string const & name)
+{
+	_name = name;
+}
+
+void ClapTrap::setHitPoints( unsigned int hp )
+{
+	_hitPoints = hp;
+}
+
+void ClapTrap::setEnergyPoints(unsigned int ep)
+{
+	_energyPoints = ep;
+}
+
+void	ClapTrap::setAttackDamage( unsigned int damage )
+{
+	_attackDamage = damage;
+}
+
+// =========================================================
+// Assignment = operator's overloading
+// =========================================================
+
+ClapTrap & ClapTrap::operator=( ClapTrap const & copy)
+{
+	setName( copy.getName() );
+	setHitPoints( copy.getHitPoints() );
+	setEnergyPoints( copy.getEnergyPoints() );
+	setAttackDamage( copy.getAttackDamage() );
+	return (*this);
+}
+
+// =========================================================
+// member functions
+// =========================================================
+
+void	ClapTrap::attack(std::string const & target)
+{
+	if (getAttackDamage() <= 0)
+		std::cout << "ClapTrap: " << getName() << " is not able to attack "
+			<< target << ", because he has " << getAttackDamage()
+			<< " attack damage points" << std::endl;
+	else if (getEnergyPoints() < 1)
+		std::cout << "ClapTrap: " << getName() << " is not able to attack "
+			<< target << ", because he has " << getEnergyPoints()
+			<< " energy points" << std::endl;
+	else if (getHitPoints() <= 0)
+		std::cout << "ClapTrap: " << getName() << " is not able to attack "
+			<< target << ", because he has " << getHitPoints()
+			<< " hit points" << std::endl;
+	if (getAttackDamage() > 0 && getEnergyPoints() > 0 && getHitPoints() > 0)
+	{
+		std::cout << "ClapTrap " << getName() << " attacks "
+			<< target << ", causing " << getAttackDamage()
+			<< " points of damage!" << std::endl;
+		setEnergyPoints(getEnergyPoints() - 1);
+	}
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	if (getHitPoints() <= 0)
+	{
+		std::cout << "It has no senseto to attack ClapTrap" << getName()
+			<< " he has " << getHitPoints() << " hit points" << std::endl;
+		return ;
+	}
+	setHitPoints(getHitPoints() - amount);
+	std::cout << "ClapTrap " << getName() << " takes "
+		<< amount << " points of damage!" << std::endl;
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	if (getEnergyPoints() < 1)
+		std::cout << "ClapTrap: " << getName() << " is not able to repair itself"
+			<< ", because he has " << getEnergyPoints()
+			<< " energy points" << std::endl;
+	else if (getHitPoints() <= 0)
+		std::cout << "ClapTrap: " << getName() << " is not able to repair itself"
+			<< ", because he has " << getHitPoints()
+			<< " hit points" << std::endl;
+	else if (getHitPoints() >= 10)
+		std::cout << "ClapTrap: " << getName() << " is not able to repair itself"
+			<< ", because he has maximum amount of hit points: " << getHitPoints() << std::endl;
+	if (getHitPoints() > 0 && getHitPoints() < 10 && getEnergyPoints() > 0 )
+	{
+		if (getHitPoints() + amount > 10)
+			setHitPoints(10);
+		else
+			setHitPoints(getHitPoints() + amount);
+		std::cout << "ClapTrap " << getName() << " recovers "
+			<< amount << " hit points!" << std::endl;
+		setEnergyPoints(getEnergyPoints() - 1);
+	}
+}
+
+// =========================================================
+// std::ostream << operator's overloading
+// =========================================================
+
+std::ostream & operator<<(std::ostream & out, ClapTrap const & ct)
+{
+	out << "ClapTrap:" << std::endl
+		<< "{" << std::endl
+		<< "\tname: " << ct.getName() << std::endl
+		<< "\thit points: " << ct.getHitPoints() << std::endl
+		<< "\tenergy points: " << ct.getEnergyPoints() << std::endl
+		<< "\tattack damage: " << ct.getAttackDamage() << std::endl
+		<< "}" << std::endl;
+	return (out);
+}
