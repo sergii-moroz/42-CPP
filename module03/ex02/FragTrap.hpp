3a21368c079e4b5a91951c7c14662627e9455cbd
sergii-moroz <70237603+sergii-moroz@users.noreply.github.com>
2024-08-31 18:03:18 +0200

    module03: ex02: CREATE FragTrap.hpp

---
+++ module03/ex02/FragTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/31 13:18:41 by smoroz            #+#    #+#             */
+/*   Updated: 2024/08/31 13:18:41 by smoroz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef Frag_TRAP_HPP
+# define Frag_TRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap
+{
+	private:
+		bool	_guardGate;
+	public:
+		FragTrap();
+		FragTrap( FragTrap const & );
+		FragTrap( std::string const & );
+		FragTrap & operator=( FragTrap const & );
+		~FragTrap();
+
+		void		attack(std::string const & target);
+		void		takeDamage(unsigned int amount);
+		void		beRepaired(unsigned int amount);
+		void		highFivesGuys();
+};
+
+std::ostream & operator<<(std::ostream & out, FragTrap const & st);
+
+#endif
