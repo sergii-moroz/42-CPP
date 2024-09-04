b0752e2d57ca182f9b36d8b37139bdce5bdcb4cc
smoroz <smoroz@student.42heilbronn.de>
2024-09-04 20:18:30 +0200

    module04: ex01: COPY Dog.hpp Dog.cpp from previous exercise. Adjust Dog class to using Brain

---
+++ module04/ex01/Dog.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 18:09:25 by smoroz            #+#    #+#             */
+/*   Updated: 2024/09/04 19:42:40 by smoroz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOG_HPP
+# define DOG_HPP
+
+# include "Animal.hpp"
+# include "Brain.hpp"
+
+class Dog : public Animal
+{
+	private:
+		Brain	*_brain;
+	public:
+		Dog();
+		~Dog();
+
+		void	makeSound() const;
+};
+
+#endif
