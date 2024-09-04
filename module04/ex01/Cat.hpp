1a4e91312508f0290ed684d41eaf9c5a3aba9e52
smoroz <smoroz@student.42heilbronn.de>
2024-09-04 20:17:27 +0200

    module04: ex01: COPY Cat.hpp Cat.cpp from previous exercise. Adjust Cat class to using Brain

---
+++ module04/ex01/Cat.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 18:09:25 by smoroz            #+#    #+#             */
+/*   Updated: 2024/09/04 11:36:07 by smoroz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CAT_HPP
+# define CAT_HPP
+
+# include "Animal.hpp"
+# include "Brain.hpp"
+
+class Cat : public Animal
+{
+	private:
+		Brain	*_brain;
+	public:
+		Cat();
+		~Cat();
+
+		void	makeSound() const;
+};
+
+#endif
