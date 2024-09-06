1fdd5fedf50c0a7cebb9e3e85630f3807177011a
smoroz <smoroz@student.42heilbronn.de>
2024-09-06 19:37:08 +0200

    module04: ex01: Refine Brain class

--- module04/ex01/Brain.cpp
+++ module04/ex01/Brain.cpp
@@ -6,7 +6,7 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 10:36:13 by smoroz            #+#    #+#             */
-/*   Updated: 2024/09/04 19:02:58 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/09/06 18:51:27 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,14 @@ Brain::~Brain()
 
 Brain::Brain( Brain const & copy )
 {
-	// *this = copy;
+	std::cout << "Brain: Copy constructor called" << std::endl;
 	for (int i = 0; i < 100; i++)
 		_ideas[i].assign(copy._ideas[i]);
-	std::cout << "Brain: Copy constructor called" << std::endl;
 }
 
 Brain &	Brain::operator=(Brain const & rhs)
 {
 	std::cout << "Brain: Assignation operator alled" << std::endl;
-	if (this == &rhs)
-		return (*this);
 	for (int i = 0; i < 100; i++)
 		_ideas[i].assign(rhs._ideas[i]);
 	return (*this);
