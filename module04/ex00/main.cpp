06b5c447bd041f998ab739100e44dd01f0823029
smoroz <smoroz@student.42heilbronn.de>
2024-09-06 18:42:46 +0200

    module04: ex00: main.cpp : Refine tests

--- module04/ex00/main.cpp
+++ module04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 10:07:10 by smoroz            #+#    #+#             */
-/*   Updated: 2024/09/04 09:28:21 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/09/06 18:27:11 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,22 @@ int	main( void )
 
 	std::cout << "=== Test 2 === start" << std::endl;
 	const Cat	*cat = new Cat();
-	const Cat	*cat_copy = new Cat(*cat);
+	const Animal	*cat_copy = new Cat(*cat);
 	std::cout << "Type: " << cat->getType() << std::endl;
+	std::cout << "Type: " << cat_copy->getType() << std::endl;
 	cat->makeSound();
+	cat_copy->makeSound();
 	delete cat;
 	delete cat_copy;
 	std::cout << "=== Test 2 === end" << std::endl << std::endl;
 
 	std::cout << "=== Test 3 === start" << std::endl;
 	const Dog	*dog = new Dog();
-	const Dog	*dog_copy = new Dog(*dog);
+	const Animal	*dog_copy = new Dog(*dog);
 	std::cout << "Type: " << dog->getType() << std::endl;
+	std::cout << "Type: " << dog_copy->getType() << std::endl;
 	dog->makeSound();
+	dog_copy->makeSound();
 	delete dog;
 	delete dog_copy;
 	std::cout << "=== Test 3 === end" << std::endl << std::endl;
@@ -54,9 +58,11 @@ int	main( void )
 
 	std::cout << "=== Test 5 === start" << std::endl;
 	const WrongCat	*wcat = new WrongCat();
-	const WrongCat	*wcat_copy = new WrongCat(*wcat);
+	const WrongAnimal	*wcat_copy = new WrongCat(*wcat);
 	std::cout << "Type: " << wcat->getType() << std::endl;
+	std::cout << "Type: " << wcat_copy->getType() << std::endl;
 	wcat->makeSound();
+	wcat_copy->makeSound();
 	delete wcat;
 	delete wcat_copy;
 	std::cout << "=== Test 5 === end" << std::endl << std::endl;
