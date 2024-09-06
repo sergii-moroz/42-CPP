db8f0bc70ad1cee88bec85a0b624dec30ad05599
smoroz <smoroz@student.42heilbronn.de>
2024-09-06 18:39:15 +0200

    module04: ex00: Cat.hpp Cat.cpp formatting changes. Add parent constructor calls

--- module04/ex00/Cat.cpp
+++ module04/ex00/Cat.cpp
@@ -6,7 +6,7 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 18:08:53 by smoroz            #+#    #+#             */
-/*   Updated: 2024/09/04 08:27:36 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/09/06 18:10:24 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@
 // constructor
 // =========================================================
 
-Cat::Cat()
+Cat::Cat() : Animal()
 {
 	setType("Cat");
 	std::cout << "Cat: default constructor called" << std::endl;
 }
 
-Cat::Cat( Cat const & copy )
+Cat::Cat( Cat const & copy ) : Animal(copy)
 {
-	*this = copy;
 	std::cout << "Cat: Copy constructor called" << std::endl;
+	*this = copy;
 }
 
 // =========================================================
@@ -41,10 +41,10 @@ Cat::~Cat()
 // Assignment = operator's overloading
 // =========================================================
 
-Cat &	Cat::operator=( Cat const & ref )
+Cat &	Cat::operator=(Cat const & ref)
 {
 	std::cout << "Cat: Assignation operator called" << std::endl;
-	setType( ref.getType() );
+	setType(ref.getType());
 	return (*this);
 }
 
