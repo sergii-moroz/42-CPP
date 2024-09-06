d2613cb3ab61529e9e0785e31ade832053d0f070
smoroz <smoroz@student.42heilbronn.de>
2024-09-06 20:03:18 +0200

    module04: ex02: Copy Dog class from previous exercise. Rename Animal to AAnimal

---
+++ module04/ex02/Dog.cpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 18:08:53 by smoroz            #+#    #+#             */
+/*   Updated: 2024/09/06 20:02:23 by smoroz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+
+// =========================================================
+// constructor
+// =========================================================
+
+Dog::Dog() : AAnimal(), _brain(new Brain())
+{
+	setType("Dog");
+	std::cout << "Dog: default constructor called" << std::endl;
+}
+
+Dog::Dog( Dog const & copy ) : AAnimal(copy), _brain(new Brain(*copy._brain))
+{
+	std::cout << "Dog: Copy constructor called" << std::endl;
+}
+
+// =========================================================
+// destructor
+// =========================================================
+
+Dog::~Dog()
+{
+	delete _brain;
+	std::cout << "Dog: Destructor called" << std::endl;
+}
+
+// =========================================================
+// Assignment = operator's overloading
+// =========================================================
+
+Dog &	Dog::operator=(Dog const & ref)
+{
+	std::cout << "Dog: Assignation operator called" << std::endl;
+	setType(ref.getType());
+	return (*this);
+}
+
+// =========================================================
+// member's function
+// =========================================================
+
+void	Dog::makeSound() const
+{
+	std::cout << getType() << ": Woof-Woof" << std::endl;
+}
---
+++ module04/ex02/Dog.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 18:09:25 by smoroz            #+#    #+#             */
+/*   Updated: 2024/09/06 20:01:20 by smoroz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOG_HPP
+# define DOG_HPP
+
+# include "AAnimal.hpp"
+# include "Brain.hpp"
+
+class Dog : public AAnimal
+{
+	public:
+		Dog();
+		Dog(Dog const & copy);
+		~Dog();
+
+		Dog &	operator=(Dog const &ref);
+		void	makeSound() const;
+	private:
+		Brain	*_brain;
+};
+
+#endif
