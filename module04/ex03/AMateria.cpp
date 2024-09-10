c30f7e8d6fb2e455e60b3bbf23e25d5c595fa2a4
smoroz <smoroz@student.42heilbronn.de>
2024-09-10 13:05:00 +0200

    module04: ex03: Add Amateria.cpp

---
+++ module04/ex03/AMateria.cpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 09:43:34 by smoroz            #+#    #+#             */
+/*   Updated: 2024/09/10 10:13:38 by smoroz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AMateria.hpp"
+
+// =========================================================
+// constructors
+// =========================================================
+
+AMateria::AMateria() : _type("AMateria")
+{
+	std::cout << "AMateria: Default constructor called" << std::endl;
+}
+
+AMateria::AMateria(AMateria const & copy)
+{
+	*this = copy;
+	std::cout << "AMateria: Copy constructor called" << std::endl;
+}
+
+// =========================================================
+// destructor
+// =========================================================
+
+AMateria::~AMateria()
+{
+	std::cout << "AMateria: Destructor called" << std::endl;
+}
+
+// =========================================================
+// getters / setters
+// =========================================================
+
+std::string const &	AMateria::getType() const
+{
+	return (_type);
+}
+
+void	AMateria::setType(std::string const & type)
+{
+	_type = type;
+}
+
+// =========================================================
+// Assignment = operator's overloading
+// =========================================================
+
+AMateria &	AMateria::operator=(AMateria const & ref)
+{
+	std::cout << "AMateria: Assignment operator called" << std::endl;
+	setType(getType());
+	return (*this);
+}
--- module04/ex03/AMateria.hpp
+++ module04/ex03/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 08:21:37 by smoroz            #+#    #+#             */
-/*   Updated: 2024/09/10 08:42:27 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/09/10 10:15:09 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,16 @@ class AMateria
 	public:
 		AMateria();
 		AMateria(AMateria const & copy);
-		~AMateria();
-		AMateria &	operator=(AMateria const & ref);
-		std::string const & getType() const;
+		virtual ~AMateria();
+
+		AMateria &			operator=(AMateria const & ref);
+		std::string const &	getType() const;
+		void				setType(std::string const & type);
 		virtual AMateria	*clone() = 0;
 		virtual void		use(ICharacter & target);
-	protected:
 
+	protected:
+		std::string	_type;
 };
 
 #endif
