7610c4657f6d9e503e301036c24252a642dc7910
Sergii Moroz <smoroz@2-D-8.42heilbronn.de>
2024-09-17 13:56:27 +0200

    module05: ex00: implement basic function for Bureaucrat: constructor destructor getter, increment, decrement, assignment operator overloading

--- module05/ex00/Bureaucrat.cpp
+++ module05/ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 13:06:41 by smoroz            #+#    #+#             */
-/*   Updated: 2024/09/17 13:17:01 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/09/17 13:53:18 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,76 @@
 // constructors
 // =========================================================
 
+Bureaucrat::Bureaucrat()
+{
+	std::cout << "Bureaucrat[ " << getName()
+		<< " ]: Default constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name), _grade(grade)
+{
+	std::cout << "Bureaucrat [ " << getName()
+		<< " ]: named constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const & copy) : _name(copy.getName()), _grade(copy.getGrade())
+{
+	std::cout << "Bureaucrat [ " << getName()
+		<< " ]: Copy constructor called" << std::endl;
+}
+
+// =========================================================
+// destructor
+// =========================================================
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat [ " << getName()
+		<< " ]: destructor called" << std::endl;
+}
+
+// =========================================================
+// operator's overloading
+// =========================================================
+
+Bureaucrat &	Bureaucrat::operator=(Bureaucrat const & rhs)
+{
+	if (this == &rhs)
+		std::cout << "Bureaucrat [ " << getName()
+			<< " ]: no need to use assignment operator(=)" << std::endl;
+	else
+	{
+		std::cout << "Bureaucrat [ " << getName()
+			<< " ]: Assignment operator called" << std::endl;
+		_grade = rhs.getGrade();
+	}
+	return (*this);
+}
+
+// =========================================================
+// getters / setters
+// =========================================================
+
+std::string	Bureaucrat::getName(void) const
+{
+	return (_name);
+}
+
+int	Bureaucrat::getGrade(void) const
+{
+	return (_grade);
+}
+
+// =========================================================
+// member function
+// =========================================================
+
+void	Bureaucrat::incGrade(void)
+{
+	_grade--;
+}
+
+void	Bureaucrat::decGrade(void)
+{
+	_grade++;
+}
--- module05/ex00/Bureaucrat.hpp
+++ module05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 13:06:45 by smoroz            #+#    #+#             */
-/*   Updated: 2024/09/17 13:15:59 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/09/17 13:53:35 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 # define BUREAUCRAT_HPP
 
 # include <string>
+# include <iostream>
 
 class Bureaucrat {
 	private:
 		const std::string	_name;
 		int					_grade;
-	public:
 		Bureaucrat();
+	public:
+		Bureaucrat(std::string name, int grade);
 		~Bureaucrat();
 		Bureaucrat(Bureaucrat const &);
 		Bureaucrat &	operator=(Bureaucrat const &);
 		std::string		getName(void) const;
 		int				getGrade(void) const;
-		Bureaucrat & operator+();
-		Bureaucrat & operator-();
-		Bureaucrat & operator+(int);
-		Bureaucrat & operator-(int);
+		void			incGrade(void);
+		void			decGrade(void);
 };
 
 #endif
