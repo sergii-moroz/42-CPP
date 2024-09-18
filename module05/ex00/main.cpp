1a1ddeea38ee77a9bc63d914958946fc79b08702
Sergii Moroz <smoroz@2-E-7.42heilbronn.de>
2024-09-18 15:18:18 +0200

    module05: ex00: add test to main.cpp

--- module05/ex00/main.cpp
+++ module05/ex00/main.cpp
@@ -6,16 +6,89 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 13:05:38 by smoroz            #+#    #+#             */
-/*   Updated: 2024/09/18 13:07:33 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/09/18 15:16:16 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+void	testCreateBureaucrat(int grade, int testId)
+{
+	std::cout
+		<< MAGENTA << std::endl
+		<< "==========================================" << std::endl
+		<< "Test " << testId << " : "
+		<< CYAN << "Create bureaucrat with grade " << grade << std::endl
+		<< MAGENTA << "=========================================="
+		<< RESET << std::endl;
+	try
+	{
+		Bureaucrat	b("bob", grade);
+		b.display();
+	}
+	catch(std::exception const & e)
+	{
+		std::cerr << RED << "ERROR: " << e.what() << RESET <<std::endl;
+	}
+}
+
+void	testIncrementBureaucrat(int grade, int testId)
+{
+	std::cout
+		<< MAGENTA << std::endl
+		<< "==========================================" << std::endl
+		<< "Test " << testId << " : "
+		<< CYAN << "Test increment" << std::endl
+		<< MAGENTA << "=========================================="
+		<< RESET << std::endl;
+	try
+	{
+		Bureaucrat	b("bob", grade);
+		b.display();
+		b.incGrade();
+		b.display();
+		b.incGrade();
+		b.display();
+	}
+	catch(std::exception const & e)
+	{
+		std::cerr << RED << "ERROR: " << e.what() << RESET <<std::endl;
+	}
+}
+
+void	testDecrementBureaucrat(int grade, int testId)
+{
+	std::cout
+		<< MAGENTA << std::endl
+		<< "==========================================" << std::endl
+		<< "Test " << testId << " : "
+		<< CYAN << "Test decrement" << std::endl
+		<< MAGENTA << "=========================================="
+		<< RESET << std::endl;
+	try
+	{
+		Bureaucrat	b("bob", grade);
+		b.display();
+		b.decGrade();
+		b.display();
+		b.decGrade();
+		b.display();
+	}
+	catch(std::exception const & e)
+	{
+		std::cerr << RED << "ERROR: " << e.what() << RESET <<std::endl;
+	}
+}
+
 int	main(void)
 {
-	Bureaucrat	b("bob", 1);
+	int	testId = 1;
 
-	b.display();
+	testCreateBureaucrat(-2, testId++);
+	testCreateBureaucrat(1, testId++);
+	testCreateBureaucrat(7, testId++);
+	testCreateBureaucrat(255, testId++);
+	testIncrementBureaucrat(2, testId++);
+	testDecrementBureaucrat(149, testId++);
 	return (0);
 }
