1905c95ff9c4b823be5bba129d5fc0f6591114f5
Sergii Moroz <smoroz@2-F-8.42heilbronn.de>
2024-09-25 15:33:54 +0200

    module05: ex02: COPY main.cpp Makefile from previous exercise

---
+++ module05/ex02/main.cpp
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/24 19:21:25 by smoroz            #+#    #+#             */
+/*   Updated: 2024/09/25 15:27:48 by smoroz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AForm.hpp"
+
+void	testCreateForm(int grade, int testId)
+{
+	std::cout
+		<< MAGENTA << std::endl
+		<< "==========================================" << std::endl
+		<< "Test " << testId << " : "
+		<< CYAN << "Create Form with grade4Sign " << grade << std::endl
+		<< MAGENTA << "=========================================="
+		<< RESET << std::endl;
+	try
+	{
+		AForm	f("c25", grade, 1);
+		std::cout << f << std::endl;
+	}
+	catch(std::exception const & e)
+	{
+		std::cerr << RED << "ERROR: " << e.what() << RESET <<std::endl;
+	}
+}
+
+void	testCreateForm2(int grade, int testId)
+{
+	std::cout
+		<< MAGENTA << std::endl
+		<< "==========================================" << std::endl
+		<< "Test " << testId << " : "
+		<< CYAN << "Create Form with grade4Exec " << grade << std::endl
+		<< MAGENTA << "=========================================="
+		<< RESET << std::endl;
+	try
+	{
+		AForm	f("c25", 150, grade);
+		std::cout << f << std::endl;
+	}
+	catch(std::exception const & e)
+	{
+		std::cerr << RED << "ERROR: " << e.what() << RESET <<std::endl;
+	}
+}
+
+void	testSignForm(int formGrade, int bureaucratGrade, int testId)
+{
+	std::cout
+		<< MAGENTA << std::endl
+		<< "==========================================" << std::endl
+		<< "Test " << testId << " : "
+		<< CYAN << "Create Form with grade " << formGrade << std::endl
+		<< CYAN << "Create Bureaucrat with grade " << bureaucratGrade << std::endl
+		<< CYAN << "Bureaucrat try to sign Form " << std::endl
+		<< MAGENTA << "=========================================="
+		<< RESET << std::endl;
+	try
+	{
+		Bureaucrat	max("Max", bureaucratGrade);
+		AForm	f("d3", formGrade, formGrade);
+		max.display();
+		std::cout << f << std::endl;
+
+		std::cout << BLACK << "try to sign" << RESET << std::endl;
+		max.signForm(f);
+		std::cout << BLACK << "try to sign second time" << RESET << std::endl;
+		max.signForm(f);
+	}
+	catch(std::exception const & e)
+	{
+		std::cerr << RED << "ERROR: " << e.what() << RESET <<std::endl;
+	}
+}
+
+int	main(void)
+{
+	int	testId = 1;
+
+	testCreateForm(-1, testId++);
+	testCreateForm(1, testId++);
+	testCreateForm(12, testId++);
+	testCreateForm(150, testId++);
+	testCreateForm(151, testId++);
+
+	testCreateForm2(-1, testId++);
+	testCreateForm2(1, testId++);
+	testCreateForm2(12, testId++);
+	testCreateForm2(150, testId++);
+	testCreateForm2(151, testId++);
+
+	testSignForm(5, 12, testId++);
+	testSignForm(12, 3, testId++);
+
+	return (0);
+}
