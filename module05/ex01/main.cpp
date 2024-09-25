9208a58a97508b8197cce2248cbeba9e16616567
Sergii Moroz <smoroz@2-F-8.42heilbronn.de>
2024-09-25 13:05:08 +0200

    module05: ex01: add testSignForm function

--- module05/ex01/main.cpp
+++ module05/ex01/main.cpp
@@ -6,11 +6,10 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 19:21:25 by smoroz            #+#    #+#             */
-/*   Updated: 2024/09/25 10:50:35 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/09/25 13:00:57 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "Bureaucrat.hpp"
 #include "Form.hpp"
 
 void	testCreateForm(int grade, int testId)
@@ -53,6 +52,34 @@ void	testCreateForm2(int grade, int testId)
 	}
 }
 
+void	testSignForm(int formGrade, int bureaucratGrade, int testId)
+{
+	std::cout
+		<< MAGENTA << std::endl
+		<< "==========================================" << std::endl
+		<< "Test " << testId << " : "
+		<< CYAN << "Create Form with grade " << formGrade << std::endl
+		<< CYAN << "Create Bureaucrat with grade " << bureaucratGrade << std::endl
+		<< CYAN << "Bureaucrat try to sign Form " << std::endl
+		<< MAGENTA << "=========================================="
+		<< RESET << std::endl;
+	try
+	{
+		Bureaucrat	max("Max", bureaucratGrade);
+		Form	f("d3", formGrade, formGrade);
+		max.display();
+		std::cout << f << std::endl;
+
+		f.beSigned(max);
+		// Try to sign second time
+		//f.beSigned(max);
+	}
+	catch(std::exception const & e)
+	{
+		std::cerr << RED << "ERROR: " << e.what() << RESET <<std::endl;
+	}
+}
+
 int	main(void)
 {
 	int	testId = 1;
@@ -69,19 +96,8 @@ int	main(void)
 	testCreateForm2(150, testId++);
 	testCreateForm2(151, testId++);
 
-	std::cout << YELLOW << "=== Test 3: if Bureaucrat [grade 2] could sign form that required grade 1 ===" << std::endl;
-	{
-		Bureaucrat	max("Max", 1);
-		Bureaucrat	bob("Bob", 12);
-		Form		f("a3", 6, 5);
-		std::cout << max << std::endl;
-		std::cout << bob << std::endl;
-		std::cout << f << std::endl;
-		std::cout << "Bob try to sign form" << std::endl;
-		f.beSigned(bob);
-		std::cout << "Max try to sign form" << std::endl;
-		f.beSigned(max);
-	}
-	std::cout << YELLOW << "=== Test 3: END ===" << std::endl << std::endl;
+	testSignForm(5, 12, testId++);
+	testSignForm(12, 3, testId++);
+
 	return (0);
 }
