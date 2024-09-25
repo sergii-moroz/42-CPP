f86457418c8402e7a9e7f63ecb44d571281e2040
smoroz <smoroz@student.42heilbronn.de>
2024-09-25 19:44:55 +0200

    module05: ex02: adjust Makefile, add test to main.cpp

--- module05/ex02/main.cpp
+++ module05/ex02/main.cpp
@@ -6,25 +6,40 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 19:21:25 by smoroz            #+#    #+#             */
-/*   Updated: 2024/09/25 15:27:48 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/09/25 19:38:47 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "AForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
-void	testCreateForm(int grade, int testId)
+void	testShrubberyCreationForm(std::string target, int testId)
 {
 	std::cout
 		<< MAGENTA << std::endl
 		<< "==========================================" << std::endl
 		<< "Test " << testId << " : "
-		<< CYAN << "Create Form with grade4Sign " << grade << std::endl
+		<< CYAN << "Create ShrubberyCreationForm with target " << target << std::endl
 		<< MAGENTA << "=========================================="
 		<< RESET << std::endl;
 	try
 	{
-		AForm	f("c25", grade, 1);
-		std::cout << f << std::endl;
+		std::cout << MAGENTA << "create ShrubberyForm as object" << RESET << std::endl;
+		ShrubberyCreationForm	shr(target);
+		ShrubberyCreationForm	*pShrubbery = NULL;
+		std::cout << std::endl;
+
+		std::cout << MAGENTA << "create ShrubberyForm as AForm *" << RESET << std::endl;
+		AForm	*f = NULL;
+		f = new ShrubberyCreationForm(target);
+		std::cout << *f << std::endl;
+		delete f;
+		std::cout << std::endl;
+
+		std::cout << MAGENTA << "create ShrubberyForm with copy constructor as ShrubberyCreationForm *" << RESET << std::endl;
+		pShrubbery = new ShrubberyCreationForm(shr);
+		std::cout << *pShrubbery << std::endl;
+		delete pShrubbery;
+		std::cout << std::endl;
 	}
 	catch(std::exception const & e)
 	{
@@ -32,7 +47,7 @@ void	testCreateForm(int grade, int testId)
 	}
 }
 
-void	testCreateForm2(int grade, int testId)
+/*void	testCreateForm2(int grade, int testId)
 {
 	std::cout
 		<< MAGENTA << std::endl
@@ -50,30 +65,35 @@ void	testCreateForm2(int grade, int testId)
 	{
 		std::cerr << RED << "ERROR: " << e.what() << RESET <<std::endl;
 	}
-}
+}//*/
 
-void	testSignForm(int formGrade, int bureaucratGrade, int testId)
+void	testShrubberySignForm(int bureaucratGrade, int testId)
 {
 	std::cout
 		<< MAGENTA << std::endl
 		<< "==========================================" << std::endl
 		<< "Test " << testId << " : "
-		<< CYAN << "Create Form with grade " << formGrade << std::endl
+		<< CYAN << "Create ShrubberyForm" << std::endl
 		<< CYAN << "Create Bureaucrat with grade " << bureaucratGrade << std::endl
-		<< CYAN << "Bureaucrat try to sign Form " << std::endl
+		<< CYAN << "Bureaucrat try to sign ShrubberyForm " << std::endl
+		<< CYAN << "Bureaucrat try to execute ShrubberyForm " << std::endl
 		<< MAGENTA << "=========================================="
 		<< RESET << std::endl;
 	try
 	{
 		Bureaucrat	max("Max", bureaucratGrade);
-		AForm	f("d3", formGrade, formGrade);
 		max.display();
-		std::cout << f << std::endl;
+		AForm	*f = NULL;
+		f = new ShrubberyCreationForm("home");
+		std::cout << *f << std::endl;
 
 		std::cout << BLACK << "try to sign" << RESET << std::endl;
-		max.signForm(f);
+		max.signForm(*f);
 		std::cout << BLACK << "try to sign second time" << RESET << std::endl;
-		max.signForm(f);
+		max.signForm(*f);
+
+		std::cout << BLACK << "try to execute" << RESET << std::endl;
+		max.executeForm(*f);
 	}
 	catch(std::exception const & e)
 	{
@@ -85,8 +105,12 @@ int	main(void)
 {
 	int	testId = 1;
 
-	testCreateForm(-1, testId++);
-	testCreateForm(1, testId++);
+	testShrubberyCreationForm("home", testId++);
+	testShrubberySignForm(45, testId++);
+	testShrubberySignForm(137, testId++);
+	testShrubberySignForm(145, testId++);
+	testShrubberySignForm(150, testId++);
+	/*testCreateForm(-1, testId++);
 	testCreateForm(12, testId++);
 	testCreateForm(150, testId++);
 	testCreateForm(151, testId++);
@@ -98,7 +122,7 @@ int	main(void)
 	testCreateForm2(151, testId++);
 
 	testSignForm(5, 12, testId++);
-	testSignForm(12, 3, testId++);
+	testSignForm(12, 3, testId++);*/
 
 	return (0);
 }
