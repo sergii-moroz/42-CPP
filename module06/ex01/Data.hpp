718391a5e5b65ea7de5c473ce3ae7fae9cf8df66
smoroz <smoroz@student.42heilbronn.de>
2024-10-07 10:53:11 +0200

    module06: ex01: add operator(<<) to Data.hpp

--- module06/ex01/Data.hpp
+++ module06/ex01/Data.hpp
@@ -6,7 +6,7 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 10:06:50 by smoroz            #+#    #+#             */
-/*   Updated: 2024/10/07 10:16:15 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/10/07 10:49:27 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,18 @@
 # define DATA_HPP
 
 # include <string>
+# include <iostream>
+
+
+# define BLACK		"\033[1;30m"
+# define RED		"\033[1;31m"
+# define GREEN		"\033[1;32m"
+# define YELLOW		"\033[1;33m"
+# define BLUE		"\033[1;34m"
+# define MAGENTA	"\033[1;35m"
+# define CYAN		"\033[1;36m"
+# define WHITE		"\033[1;37m"
+# define RESET		"\033[0m"
 
 class Data
 {
@@ -23,9 +35,9 @@ class Data
 		~Data();
 		Data &		operator=(Data const &);
 
-		std::string	getName(void);
-		double		getPrice(void);
-		int			getCount(void);
+		std::string	getName(void) const;
+		double		getPrice(void) const;
+		int			getCount(void) const;
 		void		setName(std::string name);
 		void		setPrice(double price);
 		void		setCount(int count);
@@ -36,5 +48,7 @@ class Data
 		double		_price;
 		std::string	_name;
 };
+
+std::ostream &	operator<<(std::ostream &, Data const &);
 
 #endif
