0a0c51b0b2afd0b623b0de2c36ba629738081470
smoroz <smoroz@student.42heilbronn.de>
2024-10-09 20:25:09 +0200

    module07: ex01: add checking for NULL pointer in the iter funtion

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 18:04:16 by smoroz            #+#    #+#             */
-/*   Updated: 2024/10/09 20:12:50 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/10/09 20:22:29 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 # define ITER_HPP
 
 template <typename atype>
-void	iter(atype *p, int size, void (*f)(atype &))
+void	iter(atype *parray, int size, void (*f)(atype &))
 {
+	if (parray == nullptr || f == nullptr)
+		return ;
 	for (int i=0; i<size; i++)
-		f(p[i]);
+		f(parray[i]);
 }
 
 #endif
