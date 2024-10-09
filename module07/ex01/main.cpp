b4bfeed0f3606226e4b7d6042f3acf8594eb55fc
smoroz <smoroz@student.42heilbronn.de>
2024-10-09 20:41:07 +0200

    module07: ex01: add extra test for std::string type. Add test to check empty pointer

--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -6,27 +6,17 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 19:16:23 by smoroz            #+#    #+#             */
-/*   Updated: 2024/10/09 20:20:22 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/10/09 20:39:42 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "iter.hpp"
 
-/*void	pow(int x)
-{
-	std::cout << "x: " << x << ", x*x: " << x*x << std::endl;
-}
-
-void	pow(float x)
-{
-	std::cout << "x: " << x << ", x*x: " << x*x << std::endl;
-}*/
-
 template <typename T>
 void	pow(T x)
 {
-	std::cout << "x: " << x << ", x*x: " << x*x << std::endl;
+	std::cout << "x: " << x << ", x + x: " << x + x << std::endl;
 }
 
 int	main(void)
@@ -34,6 +24,7 @@ int	main(void)
 	int		intArr[] = {0, 1, 2, 3, 4};
 	float	floatArr[] = {0.0f, 1.2f, 2.4f, 3.14f, 4.8f};
 	double	doubleArr[] = {0.0, 1.6, 2.2, 3.1, 4.5};
+	std::string	sArr[] = {"ab", "bc", "cd", "de"};
 
 	std::cout << std::endl << "Integer[]" << std::endl;
 	::iter(intArr, 5, &pow);
@@ -43,5 +34,17 @@ int	main(void)
 
 	std::cout << std::endl << "Double[]" << std::endl;
 	::iter(doubleArr, 5, &pow);
+
+	std::cout << std::endl << "std::string[]" << std::endl;
+	::iter(sArr, 4, &pow);
+
+	std::cout << std::endl << "Checking for empty array's pointer (nullptr)" << std::endl;
+	int	*empty = nullptr;
+	::iter(empty, 4, &pow);
+
+	std::cout << std::endl << "Checking for empty function's pointer (nullptr)" << std::endl;
+	void (*f)(int &) = nullptr;
+	::iter(intArr, 5, f);
+
 	return (0);
 }
