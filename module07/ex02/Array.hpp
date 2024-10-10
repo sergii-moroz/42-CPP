5c38e59a2b2f60b76749c6ae2a260eb09aa1f44f
smoroz <smoroz@student.42heilbronn.de>
2024-10-10 09:24:24 +0200

    module07: ex02: write realization for default construtor, constructor with specified size, destructor. ADD colors

--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -6,23 +6,50 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 20:43:22 by smoroz            #+#    #+#             */
-/*   Updated: 2024/10/10 08:39:55 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/10/10 09:16:06 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+# include <iostream>
 # include <exception>
+
+# define BLACK		"\033[1;30m"
+# define RED		"\033[1;31m"
+# define GREEN		"\033[1;32m"
+# define YELLOW		"\033[1;33m"
+# define BLUE		"\033[1;34m"
+# define MAGENTA	"\033[1;35m"
+# define CYAN		"\033[1;36m"
+# define WHITE		"\033[1;37m"
+# define RESET		"\033[0m"
+# define HR			"====================="
 
 template <typename T>
 class Array
 {
 	public:
-		Array();
-		Array(unsigned int n);
+		// Constructors
+		Array() : _array(new T[0])
+		{
+			std::cout << BLACK << "Array: Default constructor called" << RESET << std::endl;
+		}
+
+		Array(unsigned int n) : _array(new T[n])
+		{
+			std::cout << BLACK << "Array: Consturctor with specified size ( " << n << " ) is called" << std::endl;
+		}
+
 		Array(Array const &);
-		~Array();
+
+		// Destructor
+		~Array()
+		{
+			std::cout << BLACK << "Array: Destructor called" << std::endl;
+			delete [] _array;
+		}
 
 		Array &			operator=(Array const &);
 		unsigned int &	operator[](unsigned int const &);
@@ -31,7 +58,7 @@ class Array
 		class IndexOutOfRange : public std::exception
 		{
 			public:
-				virtual const char	*what() const throw()
+				virtual const char	*what() const throw();
 		};
 
 	private:
