2f36a3582c3bb2e6edcc0cf699bd0e740fa72989
Sergii Moroz <smoroz@student.42heilbronn.de>
2024-10-15 10:20:25 +0200

    module08: ex01: Fix assignment operator

--- module08/ex01/Span.cpp
+++ module08/ex01/Span.cpp
@@ -24,28 +24,12 @@ Span::Span(): _n(0)
 Span::Span(unsigned int n) : _n(n)
 {
 	std::cout << "Span: N-constructor called (n=" << n << ")" << std::endl;
-	// for (int i=0; i<n; i++)
-	// 	_span[i] = nullptr;
 }
 
 Span::Span(Span const & copy)
 {
 	std::cout << "Span: Copy constructor called" << std::endl;
 	*this = copy;
-}
-
-Span &	Span::operator=(Span const & rhs)
-{
-	if (this != &rhs)
-	{
-		_n = rhs._n;
-		// if (_span != nullptr)
-		// 	delete [] _span;
-		// _span = new int[_n];
-		// for (int i=0; i<_n; i++)
-		// 	_span[i] = rhs._span[i];
-	}
-	return (*this);
 }
 
 // =========================================================
@@ -56,3 +40,20 @@ Span::~Span()
 {
 	std::cout << "Span: Destructor called" << std::endl;
 }
+
+// =========================================================
+// assignment operator overloading
+// =========================================================
+
+Span &	Span::operator=(Span const & rhs)
+{
+	std::cout << "Span: Assignment operator called" << std::endl;
+	if (this != &rhs)
+	{
+		_n = rhs._n;
+		_v = rhs._v;
+	}
+	return (*this);
+}
+
+
