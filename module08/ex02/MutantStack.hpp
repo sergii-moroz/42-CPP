166e121dc759e13f93dc2b6a660e34c0461a312c
Sergii Moroz <smoroz@student.42heilbronn.de>
2024-10-16 20:53:41 +0200

    module08: ex02: ADD iterators

--- module08/ex02/MutantStack.hpp
+++ module08/ex02/MutantStack.hpp
@@ -21,8 +21,8 @@
 // # define GREEN		"\033[1;32m"
 // # define YELLOW		"\033[1;33m"
 // # define BLUE		"\033[1;34m"
-// # define MAGENTA	"\033[1;35m"
-// # define CYAN		"\033[1;36m"
+# define MAGENTA	"\033[1;35m"
+# define CYAN		"\033[1;36m"
 // # define WHITE		"\033[1;37m"
 # define RESET		"\033[0m"
 
@@ -34,9 +34,18 @@ class MutantStack : public std::stack<T>
 		MutantStack(MutantStack const &);
 		~MutantStack();
 
-		MutantStack &	operator=(MutantStack const &);
+		MutantStack &									operator=(MutantStack const &);
 
-	private:
+		typedef std::stack<T>							stack;
+		typedef typename stack::container_type			container;
+		typedef typename container::iterator			iterator;
+		typedef typename container::reverse_iterator	reverse_iterator;
+
+		iterator			begin();
+		iterator			end();
+
+		reverse_iterator	rbegin();
+		reverse_iterator	rend();
 };
 
 # include "MutantStack.tpp"
