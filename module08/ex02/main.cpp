b105df266666cff8b9bd5436885c104f53a5250c
Sergii Moroz <smoroz@student.42heilbronn.de>
2024-10-17 07:52:10 +0200

    module08: ex02: Add more tests to main.cpp

--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -11,20 +11,82 @@
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
-// #include <stack>
-// #include <iostream>
+#include <string>
 
 int	main(void)
 {
-	// std::stack<int> stack;
-	MutantStack<int>	stack;
-	stack.push(99);
-	stack.push(12);
-	std::cout << stack.top() << std::endl;
-	std::cout << *(&(stack.top()) - 1) << std::endl;
+	{
+		std::cout << MAGENTA << "==========================================" << std::endl;
+		std::cout << "MutantStack<" << CYAN << "int" << MAGENTA << ">" << std::endl;
+		std::cout << "==========================================" << RESET << std::endl;
 
-	// end() = &(stack.top()) + 1
-	// begin() = &(stack.top()) - size()
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << "push 5 17: top: " << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << "pop(), size: " << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+		std::cout << "push 3 5 737 0: top: " << mstack.top() << std::endl;
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		std::cout << "mstack { ";
+		while (it != ite)
+		{
+			std::cout << *it << " ";
+			++it;
+		}
+		std::cout << "}" << std::endl;
+
+		std::stack<int> s(mstack);
+	}
+
+	{
+		std::cout << std::endl;
+		std::cout << MAGENTA << "==========================================" << std::endl;
+		std::cout << "MutantStack<" << CYAN << "std::string" << MAGENTA << ">" << std::endl;
+		std::cout << "==========================================" << RESET << std::endl;
+
+		MutantStack<std::string> mstack;
+		mstack.push("five");
+		mstack.push("seventeen");
+		std::cout << "push 5 17: top: " << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << "pop(), size: " << mstack.size() << std::endl;
+		mstack.push("three");
+		mstack.push("one");
+		mstack.push("forty two");
+		std::cout << "push() 3 1 42: top: " << mstack.top() << std::endl;
+		MutantStack<std::string>::iterator it = mstack.begin();
+		MutantStack<std::string>::iterator ite = mstack.end();
+		++it;
+		--it;
+		std::cout << "mstack { ";
+		while (it != ite)
+		{
+			std::cout << "\"" << *it << "\" ";
+			++it;
+		}
+		std::cout << "}" << std::endl;
+
+		std::cout << BLACK << "reverse" << RESET << std::endl;
+		MutantStack<std::string>::reverse_iterator rit = mstack.rbegin();
+		MutantStack<std::string>::reverse_iterator rite = mstack.rend();
+		std::cout << "mstack { ";
+		while (rit != rite)
+		{
+			std::cout << "\"" << *rit << "\" ";
+			++rit;
+		}
+		std::cout << "}" << std::endl;
+
+		std::stack<std::string> s(mstack);
+	}
 
 	return (0);
 }
