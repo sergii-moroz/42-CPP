7938592fbb36e8e72bf0121d84fd3dfc76829dae
Sergii Moroz <smoroz@student.42heilbronn.de>
2024-11-07 16:42:24 +0100

    module09: ex01: add mathematical operators

--- module09/ex01/RPN.cpp
+++ module09/ex01/RPN.cpp
@@ -40,11 +40,71 @@ RPN::~RPN()
 // Assignment operator(=)
 // ==========================================
 
-RPN::operator=(RPN const & rhs)
+RPN &	RPN::operator=(RPN const & rhs)
 {
 	if (this != &rhs)
 	{
 		// nothing to do
 	}
 	return (*this);
+}
+
+// ==========================================
+//	member functions
+// ==========================================
+
+double	RPN::calc(std::string const & s)
+{
+	std::stack<double>	stack;
+
+	char	c;
+	for (size_t i=0; i < s.length(); i++)
+	{
+		c = s.at(i);
+		if (std::isdigit(c))
+		{
+			stack.push(static_cast<double>(c) - '0');
+			std::cout  << "top: " << stack.top() << std::endl;
+		}
+		else if (std::isspace(c))
+		{
+			std::cout << c << " is space" << std::endl;
+			continue;
+		}
+		else if (RPN::isMathOperator(c))
+		{
+
+			std::cout << c << " it is operator" << std::endl;
+		}
+		else
+			std::cout << "invalid character" << std::endl;
+	}
+		// std::cout << s.at(i) << std::endl;
+	return (1.1);
+}
+
+bool	RPN::isMathOperator(char const & c)
+{
+	std::string	ops = "+-*/";
+	return (ops.find(c) != std::string::npos);
+}
+
+double	RPN::add(double const & x, double const & y)
+{
+	return (x + y);
+}
+
+double	RPN::sub(double const & x, double const & y)
+{
+	return (x - y);
+}
+
+double	RPN::mlt(double const & x, double const & y)
+{
+	return (x * y);
+}
+
+double	RPN::div(double const & x, double const & y)
+{
+	return (x / y);
 }
--- module09/ex01/RPN.hpp
+++ module09/ex01/RPN.hpp
@@ -14,16 +14,24 @@
 # define RPN_HPP
 
 # include <iostream>
+# include <stack>
+# include <string>
 
 class RPN
 {
 	public:
+		static double	calc(std::string const &);
+
+	private:
 		RPN();
 		~RPN();
 		RPN(RPN const &);
 		RPN &	operator=(RPN const &);
-
-	private:
+		static bool	isMathOperator(char const & c);
+		static double	add(double const &, double const &);
+		static double	sub(double const &, double const &);
+		static double	mlt(double const &, double const &);
+		static double	div(double const &, double const &);
 };
 
 #endif
