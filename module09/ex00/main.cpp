42fd9a50c0afbb3c298d94e0fab539d03c6e406e
Sergii Moroz <smoroz@3-D-2.42heilbronn.de>
2024-12-19 15:46:42 +0100

    module09: ex00: main.cpp check argc and print usage. Or create btc object and load DB

--- module09/ex00/main.cpp
+++ module09/ex00/main.cpp
@@ -6,13 +6,23 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 19:14:56 by smoroz            #+#    #+#             */
-/*   Updated: 2024/10/17 19:14:56 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/12/19 15:31:21 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
+	if (argc != 2)
+	{
+		BitcoinExchange::usage();
+		return (1);
+	}
+
+	BitcoinExchange	btc;
+	btc.loadDB();
+
+	std::cout << argv[0] << std::endl;
 	return (0);
 }
