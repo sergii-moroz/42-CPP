e2e0b83e99a596712875ab5c83b2f28fb42d7f17
Sergii Moroz <smoroz@3-D-2.42heilbronn.de>
2024-12-19 15:13:07 +0100

    module09: ex00: class BitcoinExchange in classical orthodox form

--- module09/ex00/BitcoinExchange.cpp
+++ module09/ex00/BitcoinExchange.cpp
@@ -6,9 +6,44 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 19:14:22 by smoroz            #+#    #+#             */
-/*   Updated: 2024/10/17 19:14:22 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/12/19 15:10:19 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
+// ==========================================
+// Constructors
+// ==========================================
+
+BitcoinExchange::BitcoinExchange()
+{
+	std::cout << BLACK << "BitcoinExchange: Default constructor called" << RESET << std::endl;
+}
+
+BitcoinExchange::BitcoinExchange(BitcoinExchange const & copy)
+{
+	std::cout << BLACK << "BitcoinExchange: Copy constructor called" << RESET << std::endl;
+	*this = copy;
+}
+
+// ==========================================
+// Destructor
+// ==========================================
+
+BitcoinExchange::~BitcoinExchange()
+{
+	std::cout << BLACK << "BitcoinExchange: Destructor called" << RESET << std::endl;
+}
+
+// ==========================================
+// Assignment operator(=)
+// ==========================================
+BitcoinExchange &	BitcoinExchange::operator=(BitcoinExchange const & rhs)
+{
+	if (this != &rhs)
+	{
+		// do something
+	}
+	return (*this);
+}
--- module09/ex00/BitcoinExchange.hpp
+++ module09/ex00/BitcoinExchange.hpp
@@ -6,13 +6,36 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 19:13:24 by smoroz            #+#    #+#             */
-/*   Updated: 2024/10/17 19:13:24 by smoroz           ###   ########.fr       */
+/*   Updated: 2024/12/19 15:10:50 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
 
+# define BLACK		"\033[1;30m"
+# define RED		"\033[1;31m"
+# define GREEN		"\033[1;32m"
+# define YELLOW		"\033[1;33m"
+# define BLUE		"\033[1;34m"
+# define MAGENTA	"\033[1;35m"
+# define CYAN		"\033[1;36m"
+# define WHITE		"\033[1;37m"
+# define RESET		"\033[0m"
 
+# include <iostream>
+# include <map>
+
+class BitcoinExchange
+{
+	public:
+		BitcoinExchange();
+		~BitcoinExchange();
+		BitcoinExchange(BitcoinExchange const &);
+		BitcoinExchange &	operator=(BitcoinExchange const &);
+
+	private:
+		std::map<std::string, double>	db;
+};
 
 #endif
